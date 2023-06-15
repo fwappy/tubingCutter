b00af9240918901293cccbab55311859547edcac
fwappy <Ezra.kainz@gmail.com>
2023-06-15 11:37:07 -0700

    more progress

--- cutter.py
+++ cutter.py
@@ -15,50 +15,58 @@ class Cutter(QtWidgets.QWidget):
         self.show() # Show the GUI
         self.cutButton.clicked.connect(self.cut)        # connect buttons
         self.resetButton.clicked.connect(self.reset)
+        self.length.valueChanged.connect(self.updateTotal)
+        self.cuts.valueChanged.connect(self.updateTotal)
+        self.setButton.clicked.connect(self.setMotorVars)
         self.serial = QtSerialPort.QSerialPort(         # open serial connection
             '/dev/ttyUSB0',
             baudRate=QtSerialPort.QSerialPort.Baud9600,
             readyRead=self.receive
         )
-        self.serial.open(QtCore.QIODevice.ReadWrite)
         self.reset()
 
     @QtCore.pyqtSlot()
     def cut(self): #make this non blocking + catch no cnnection error and display in red to consoleTextbox
         self.cutButton.setEnabled(False)
-        if self.serial.isOpen():
-            self.serial.write(self.length.text().encode())
-            self.serial.write(b'\r') #send newline
-            self.consoleTextbox.append(self.length.text()) #update textbox
-            time.sleep(5) #replace with wait till end of message - allow reset button to break
-            self.serial.write(self.cuts.text().encode())
-            self.serial.write(b'\r')
-            self.consoleTextbox.append(self.cuts.text())
-            #wait until read "complete!"
-        else:
-            self.conErr()
+        self.send(self.length.text())
+        time.sleep(1) #replace with wait till end of message - allow reset button to break
+        self.send(self.cuts.text())
+        #wait until read "complete!"
         self.cutButton.setChecked(False)
         self.cutButton.setEnabled(True)
 
     @QtCore.pyqtSlot()
     def reset(self):
-        if self.serial.isOpen():
+        if self.serial.isOpen(): #add other error handling (for example disconnected while open)
             self.serial.write(b'\x03')
         else:
-            self.conErr()
-
-    def conErr(self):
-        self.consoleTextbox.append("Error: No serial connection to cutter") #make red
+            self.serial.open(QtCore.QIODevice.ReadWrite)
+            if self.serial.isOpen():
+                self.serial.write(b'\x03')
+            else:
+                self.consoleTextbox.append("Error: No serial adapter") #make red
 
     #future: add reset cuts on blade + rest of info section
 
+    def send(self, text):
+        self.serial.write(text.encode())
+        self.serial.write(b'\r') #send newline
+        self.serial.waitForBytesWritten() # blocking
+
     @QtCore.pyqtSlot()
-    def receive(self): #this doesnt work
+    def receive(self):
         while self.serial.canReadLine():
             text = self.serial.readLine().data().decode()
-            #text = text.rstrip('\r\n')
             self.consoleTextbox.append(text)
 
+    @QtCore.pyqtSlot()
+    def updateTotal(self): #updates the total LCDNumber
+        self.total.display(self.length.value() * self.cuts.value())
+
+    @QtCore.pyqtSlot()
+    def setMotorVars(self):
+        #self.send(b'figurethisout\r')
+        print("Not Implemented")
 
 if __name__ == '__main__':
     import sys
